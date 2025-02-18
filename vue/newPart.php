c93f22fa417e84a6dd199050725570b2f1517c1c
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2025-02-18 15:19:11 +0100

    create the front of the nowPart page

--- vue/newPart.php
+++ vue/newPart.php
@@ -1,12 +1,54 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr-fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nouvelle partie</title>
 </head>
+
 <body>
-    <h1>Créé une nouvelle partie !</h1>
-    
+    <h1>Créez une nouvelle partie !</h1>
+    <form action="" method="post">
+        <fieldset>
+            <legend>Formulaire de création d'une nouvelle partie.</legend>
+            <div>
+                <label for="gameName">Nom de la partie</label>
+                <input type="text" id="gameName" name="gameName">
+            </div>
+            <div>
+                <label for="numPlayers">Nombre de joueurs</label>
+                <input type="number" min="1" max="5" step="1" id="numPlayers" name="numPlayers">
+            </div>
+            <div>
+                <p>Difficulté du jeu</p>
+                <div>
+                    <input type="radio" id="easy" name="difficulty" value="easy">
+                    <label for="easy">Facile</label>
+                </div>
+                <div>
+                    <input type="radio" id="normal" name="difficulty" value="normal">
+                    <label for="normal">Normale</label>
+                </div>
+                <div>
+                    <input type="radio" id="hard" name="difficulty" value="hard">
+                    <label for="hard">Difficile</label>
+                </div>
+                <div>
+                    <input type="radio" id="expert" name="difficulty" value="expert">
+                    <label for="expert">Expert</label>
+                </div>
+            </div>
+            <div>
+                <button id="random">Aléatoire</button>
+            </div>
+            <div>
+                <button id="create">Créer la partie !</button>
+            </div>
+            <span>Info : toute partie créée expire au bout de 24 heures.</span>
+
+        </fieldset>
+    </form>
 </body>
+
 </html>
